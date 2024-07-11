50cf0885dbcc9f179bd8689b80ff0f230ecbea8e
Arpon <a4arpon@gmail.com>
2024-07-11 22:27:42 +0600

    routes added

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,6 @@
+import { type Context, Hono } from "hono"
 import { StableRoutes } from "#routes/stable.routes.ts"
 import { exception, HTTPStatus } from "#utils/response.ts"
-import { Context, Hono } from "hono"
 
 export class Router {
   public router = new Hono()
@@ -12,18 +12,16 @@ export class Router {
 
   private stable() {
     const { stableRoutes } = new StableRoutes()
-    this.router.basePath("stable")
-      .route("/", stableRoutes)
+    this.router.basePath("stable").route("/", stableRoutes)
   }
 
   private beta() {
-    this.router.basePath("beta")
-      .all("/", (ctx: Context) => {
-        return exception(
-          ctx,
-          HTTPStatus.NotFound,
-          new Error("Beta channel not active yet"),
-        )
-      })
+    this.router.basePath("beta").all("/", (ctx: Context) => {
+      return exception(
+        ctx,
+        HTTPStatus.NotFound,
+        new Error("Beta channel not active yet"),
+      )
+    })
   }
 }
